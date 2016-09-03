a0eec9e1f8c53e4da8982bc343350148311220b5
William Quan <William@Williams-MacBook-Pro.local>
2016-09-03 00:03:22 -0700

    uh bad test?

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,6 +6,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
+#  test "should get root" do
+#    get static_pages_root_url
+#    assert_response :success
+#  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -16,7 +21,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_help_url
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
+  end
 
+  test "should get contact" do
+    get static_pages_help_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
   test "should get about" do
